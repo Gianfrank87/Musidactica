8935231ee88d9777f7976332a197e1dd3a073318
GianFrank87 <Gianfrank87@gmail.com>
2026-05-17 21:57:40 -0300

    solucion a error 404

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        games: resolve(__dirname, 'src/games/index.html'),
+        ritmo: resolve(__dirname, 'src/games/ritmo/index.html'),
+        instrumentos: resolve(__dirname, 'src/games/instrumentos/index.html'),
+        melodia: resolve(__dirname, 'src/games/melodia/index.html'),
+        trivia: resolve(__dirname, 'src/games/trivia/index.html'),
+        virtuales: resolve(__dirname, 'src/games/virtuales/index.html'),
+      }
+    }
+  }
+})
