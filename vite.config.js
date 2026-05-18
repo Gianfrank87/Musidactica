5a212f6fec5c158f9e650a463374a16660dcaa06
GianFrank87 <Gianfrank87@gmail.com>
2026-05-17 22:23:18 -0300

    Build configurada compilacion multi pagina correcta con vite y netlify

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,16 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        games: resolve(__dirname, 'src/games/index.html'),
-        ritmo: resolve(__dirname, 'src/games/ritmo/index.html'),
-        instrumentos: resolve(__dirname, 'src/games/instrumentos/index.html'),
-        melodia: resolve(__dirname, 'src/games/melodia/index.html'),
-        trivia: resolve(__dirname, 'src/games/trivia/index.html'),
-        virtuales: resolve(__dirname, 'src/games/virtuales/index.html'),
+        main:        resolve(__dirname, 'index.html'),
+        games:       resolve(__dirname, 'src/games/index.html'),
+        ritmo:       resolve(__dirname, 'src/games/ritmo/index.html'),
+        instrumentos:resolve(__dirname, 'src/games/instrumentos/index.html'),
+        melodia:     resolve(__dirname, 'src/games/melodia/index.html'),
+        trivia:      resolve(__dirname, 'src/games/trivia/index.html'),
+        virtuales:   resolve(__dirname, 'src/games/virtuales/index.html'),
       }
     }
   }
